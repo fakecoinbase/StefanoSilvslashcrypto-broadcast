091f3f95b5e746993065c619b4f1562047ac61dc
Stefano Silvestrini <silver.stefano@gmail.com>
2020-04-08 17:59:48 +0200

    second grid structured and conversion list styled

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ function App() {
 	return (
 		<div className="App">
 			<div className="topbar"></div>
-			<div className="grid">
+
+			<div className="first-grid">
 				<div className="mainNews"></div>
 				<div className="secondaryNews"></div>
 				<div></div>
 				<div></div>
 				<div></div>
 				<div></div>
+			</div>
+
+			<div className="second-grid">
+				<div></div>
+				<div></div>
+				<ul>
+					<li>1 BTC = $13.2232</li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
 			</div>
 		</div>
 	);
