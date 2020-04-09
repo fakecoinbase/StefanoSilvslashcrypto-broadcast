555a3e8ead2d71aef0686816f8c7af0ee2acf11c
Stefano Silvestrini <silver.stefano@gmail.com>
2020-04-09 11:31:17 +0200

    structure of the app improved and styled

--- src/App.js
+++ src/App.js
@@ -19,18 +19,109 @@ function App() {
 				<div>
 					<h3>RECENT NEWS</h3>
 					<div className="grid_recent">
-						<div className="mainNews_recent"></div>
-						<div className="otherNews_recent"></div>
+						<div className="mainNews_recent">
+							<div className="mainNews_recent_img"></div>
+							<div className="mainNews_recent_content">
+								<h3>Title goes here</h3>
+								<small>
+									by <strong>David Palmer</strong>, Jan 4th 2020
+								</small>
+								<p>
+									Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+									accusantium doloremque laudantium, totam rem aperiam, eaque
+									ipsa quae ab illo inventore.
+								</p>
+							</div>
+						</div>
+						<ul className="otherNews_recent">
+							<li className="li_grid">
+								<div className="otherNews_recent_image"></div>
+								<div>
+									<h4>Here it goes the title</h4>
+									<small>
+										by <strong>David Palmer</strong>, Jan 4th 2020
+									</small>
+								</div>
+							</li>
+							<li className="li_grid">
+								<div className="otherNews_recent_image"></div>
+								<div>
+									<h4>Here it goes the title</h4>
+									<small>
+										by <strong>David Palmer</strong>, Jan 4th 2020
+									</small>
+								</div>
+							</li>
+							<li className="li_grid">
+								<div className="otherNews_recent_image"></div>
+								<div>
+									<h4>Here it goes the title</h4>
+									<small>
+										by <strong>David Palmer</strong>, Jan 4th 2020
+									</small>
+								</div>
+							</li>
+							<li className="li_grid">
+								<div className="otherNews_recent_image"></div>
+								<div>
+									<h4>Here it goes the title</h4>
+									<small>
+										by <strong>David Palmer</strong>, Jan 4th 2020
+									</small>
+								</div>
+							</li>
+						</ul>
 					</div>
 				</div>
-				<ul>
-					<li>1 BTC = $13.2232</li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-				</ul>
+				<div className="second_grid_third_column">
+					<ul class="conversions">
+						<li>1 BTC = $13.2232</li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+					<h3>POPULAR NEWS</h3>
+					<ul className="otherNews_recent">
+						<li className="li_grid">
+							<div className="otherNews_recent_image"></div>
+							<div>
+								<h4>Here it goes the title</h4>
+								<small>
+									by <strong>David Palmer</strong>, Jan 4th 2020
+								</small>
+							</div>
+						</li>
+						<li className="li_grid">
+							<div className="otherNews_recent_image"></div>
+							<div>
+								<h4>Here it goes the title</h4>
+								<small>
+									by <strong>David Palmer</strong>, Jan 4th 2020
+								</small>
+							</div>
+						</li>
+						<li className="li_grid">
+							<div className="otherNews_recent_image"></div>
+							<div>
+								<h4>Here it goes the title</h4>
+								<small>
+									by <strong>David Palmer</strong>, Jan 4th 2020
+								</small>
+							</div>
+						</li>
+						<li className="li_grid">
+							<div className="otherNews_recent_image"></div>
+							<div>
+								<h4>Here it goes the title</h4>
+								<small>
+									by <strong>David Palmer</strong>, Jan 4th 2020
+								</small>
+							</div>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
